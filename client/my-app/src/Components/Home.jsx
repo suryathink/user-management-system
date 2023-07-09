2d00031555c5df12bfb9d26960f558deb771e832
Surya Prakash Singh <suryaprakashsingh110@gmail.com>
2023-07-09 18:01:30 +0530

    Table in Home Page Created

---
+++ client/my-app/src/Components/Home.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { useEffect } from "react";
+import Button from "react-bootstrap/Button";
+
+// import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+
+
+const Home = () => {
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    try {
+      let result = await fetch(`http://localhost:8080/users`);
+      result = await result.json();
+      // console.log(result.users);
+      setData(result.users);
+      console.log("Inside State", data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <div>
+    <br/>
+      <Button variant="primary" size="lg" active>
+        Add User
+      </Button>{" "}
+      <br/>
+      <div>
+
+      <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650}} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="center">Serial Number</TableCell>
+            <TableCell align="center">_id</TableCell>
+            <TableCell align="center">Name</TableCell>
+            <TableCell align="center">Email Id</TableCell>
+            <TableCell align="center">Phone Number</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((row,i) => (
+            <TableRow
+              key={i+1}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell align="center">{i+1}</TableCell>
+              <TableCell align="center" component="th" scope="row">
+                {row._id}
+              </TableCell>
+              <TableCell align="center">{row.name}</TableCell>
+              <TableCell align="center">{row.email}</TableCell>
+              <TableCell align="center">{row.phone}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+      </div>
+      <br/>
+    </div>
+  );
+};
+
+export default Home;
